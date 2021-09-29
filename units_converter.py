2e801e7b829130433cfee3a2263e5f274565c071
Polina Guro <polinaguro@gmail.com>
2021-09-29 19:39:05 +0300

    modified converter in first branch

--- units_converter.py
+++ units_converter.py
@@ -5,5 +5,5 @@ if temperature == 1:
   temperature_celsius = float(input('Введите значение температуры по Цельсию:\n'))
   print(temperature_celsius, 'градусов по Цельсию =', round((((9/5) * temperature_celsius +32)), 2),'градусов по Фаренгейту')
 elif temperature == 2:
-  temperature_сelsius = float(input('Сколько градусов по Цельсию? :\n'))
+  temperature_сelsius = float(input('Введите значение температуры по Цельсию:\n'))
   print(temperature_сelsius, 'градусов по Цельсию =', round((temperature_сelsius + 273.15), 2),'градусов по Кельвину')
